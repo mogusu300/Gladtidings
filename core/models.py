dada4c22bd0d219ce522f048f99e201f55f7a7e0
mogusu300 <66074163+mogusu300@users.noreply.github.com>
2024-05-22 09:51:53 +0200

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Institution(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    public = models.BooleanField(default=True)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE, null=True, blank=True)
+
+class Topic(models.Model):
+    course = models.ForeignKey(Course, related_name='topics', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+class Enrollment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Certificate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    issued_at = models.DateTimeField(auto_now_add=True)
