5c68d6c63f77a3dc94405538659e13e624e02d42
DevMusengah <123998676+Musengah@users.noreply.github.com>
2024-05-27 14:22:27 +0200

    Update api.py
    Updated the api.py for topics

--- core/api.py
+++ core/api.py
@@ -1,20 +1,14 @@
 # core/api.py
-from ninja import NinjaAPI, Router
-from .models import CustomUser
-from .scheme import UserCreateSchema, UserSchema
+from ninja import NinjaAPI
+from typing import List
+from django.shortcuts import get_object_or_404
+from .models import Course, Topic
+from .schema import TopicSchema
 
 api = NinjaAPI()
 
-router = Router()
-
-@router.post("/register", response=UserSchema)
-def register(request, data: UserCreateSchema):
-    user = CustomUser.objects.create_user(
-        username=data.username,
-        email=data.email,
-        password=data.password,
-        role=data.role
-    )
-    return user
-
-api.add_router("/", router)  # Add the router to the API instance
+@api.get("/courses/{course_id}/topics", response=List[TopicSchema])
+def list_topics(request, course_id: int):
+    course = get_object_or_404(Course, id=course_id)
+    topics = Topic.objects.filter(course=course)
+    return topics
