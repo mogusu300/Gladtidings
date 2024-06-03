84f06aa0f77b2b7ed7c8db9b629d6c3e81b0d7cb
Gabriel-Chilatu-Sinkala <120474088+Gabriel-Chilatu-Sinkala@users.noreply.github.com>
2024-06-03 00:15:22 +0000

    Pending changes exported from your codespace

--- core/api.py
+++ core/api.py
@@ -1,20 +1,24 @@
 # core/api.py
 from ninja import NinjaAPI, Router
-from .models import CustomUser
-from .scheme import UserCreateSchema, UserSchema
+from .models import Course, Enrollment
+from .scheme import EnrollmentSchema, CourseSchema
 
 api = NinjaAPI()
 
 router = Router()
 
-@router.post("/register", response=UserSchema)
-def register(request, data: UserCreateSchema):
-    user = CustomUser.objects.create_user(
-        username=data.username,
-        email=data.email,
-        password=data.password,
-        role=data.role
-    )
-    return user
+@router.post("/enrollments/", response=EnrollmentSchema)
+def enroll_user_in_course(request, data: EnrollmentSchema):
+    try:
+        course = Course.objects.get(id=data.course_id)
+        enrollment = Enrollment.objects.create(user=request.user, course=course)
+        return enrollment
+    except Course.DoesNotExist:
+        return {"error": "Course not found"}
 
-api.add_router("/", router)  # Add the router to the API instance
+api.add_router("/api/v1/", router) 
+
+
+
+
+
